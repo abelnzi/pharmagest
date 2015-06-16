77b002d550b44da00f073c9bbf0d8c25c2b79b2e
Abel <abelnzi@gmail.com>
2015-06-16 18:25:11 +0200

    mise en place des liens

---
+++ omod/src/main/java/org/openmrs/module/pharmagest/extension/html/PharmaGestGutterLink.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package org.openmrs.module.pharmagest.extension.html;
+
+import org.openmrs.module.web.extension.LinkExt;
+
+/**
+ * @author Abel
+ *
+ */
+public class PharmaGestGutterLink extends LinkExt {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openmrs.module.web.extension.LinkExt#getLabel()
+	 */
+	@Override
+	public String getLabel() {
+
+		return "pharmagest.generate.gutterlink";
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openmrs.module.web.extension.LinkExt#getRequiredPrivilege()
+	 */
+	@Override
+	public String getRequiredPrivilege() {
+
+		return "Manage Generateur ID";
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openmrs.module.web.extension.LinkExt#getUrl()
+	 */
+	@Override
+	public String getUrl() {
+		return "module/generateurid/manage.form";
+	}
+
+}
